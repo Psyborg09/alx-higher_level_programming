6503a66c9dd5c4b975373ad90f2e1d3a0c8da8d6
psyborg09 <psyborg09@github.com>
2023-08-20 20:07:11 +0000

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
 
     if len(sys.argv) == 1:
         print("{} arguments.".format(0))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(1))
+        print("{}: {}".format(1, (sys.argv[1])))
     else:
         print("{} argument:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
