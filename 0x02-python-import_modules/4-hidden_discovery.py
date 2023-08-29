674c40c38e38b5859cf4f92fa38cf601c6d01e74
psyborg09 <psyborg09@github.com>
2023-08-29 17:22:37 +0000

    done with debugging

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     import hidden_4
 
     name = dir(hidden_4)
-    for i in name:
-        if i[0:2] != "__":
+    for nam in name:
+        if nam[0:2] != "__":
             print("{}".format(i)) 
